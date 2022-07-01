df2a78d35af1e669421c00b8bc809c79465bc7c5
Amehiz <abdallahmehiz0@gmail.com>
2022-07-01 03:19:05 +0100

    Add Simple Counter

--- app/src/main/java/me/mehiz/learningandroid/MainActivity.kt
+++ app/src/main/java/me/mehiz/learningandroid/MainActivity.kt
@@ -2,10 +2,20 @@ package me.mehiz.learningandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buton = findViewById<Button>(R.id.Clicker)
+        val clicks = findViewById<TextView>(R.id.Counter)
+        var totalClicks = 0
+        buton.setOnClickListener{
+            totalClicks++
+        clicks.text = totalClicks.toString()
+        }
     }
 }
